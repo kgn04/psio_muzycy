ddaa9d8f72dcb5a3059ddb96d35ad2b8077e2081
komp15 <michal.komp@interia.eu>
2021-12-07 00:26:34 +0100

    Stworzenie klas krytyk, piosenka, pakiet repertuar

---
+++ cw_l6_z2/src/repertuar/Krytyk.java
@@ -0,0 +1,19 @@
+package repertuar;
+
+import muzycy.Osoba;
+
+public class Krytyk extends Osoba{
+
+	
+	//TODO ficzer krytyka
+	public Krytyk(String imie, String nazwisko) {
+		super(imie, nazwisko);
+
+		
+	}
+
+	
+	
+	
+	
+}
