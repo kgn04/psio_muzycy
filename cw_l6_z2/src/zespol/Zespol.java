a864d016cd371d1c233a5b6c74c6370c6d4e97dc
SzymonKielich <szymon.kielich@gmail.com>
2021-12-13 17:45:42 +0100

    poprawa algorytmu

--- cw_l6_z2/src/zespol/Zespol.java
+++ cw_l6_z2/src/zespol/Zespol.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import muzycy.Muzyk;
+import muzycy.Osoba;
 import muzycy.Pianista;
 import muzycy.Saksofonista;
 import muzycy.Wokalista;
@@ -49,7 +50,7 @@ public class Zespol {
         List <Wokalista> wokalisci = arrayWokalisci(muzycy);
         List <Pianista> pianisci = arrayPianisci(muzycy);
         List <Saksofonista> saksofonisci  = arraySaksofonisci(muzycy);
-        Zespol zespol = wybierzZespol( technicy , wokalisci , pianisci , saksofonisci , budzet );
+        Zespol zespol = wybierzZespol2( technicy , wokalisci , pianisci , saksofonisci , budzet );
         
         showOptimalTeam(zespol,budzet);
 		
@@ -85,6 +86,60 @@ public class Zespol {
 
         else return new Zespol ( technicy.get(iT) , wokalisci.get(iW) , pianisci.get(iP) , saksofonisci.get(iS) );
         
+    }
+	
+	public static Zespol wybierzZespol2(List<Technik> technicy , List <Wokalista> wokalisci , List <Pianista> pianisci , List <Saksofonista> saksofonisci , int budzet )
+    {
+		
+        List <Osoba> osoby = null;
+
+        osoby.addAll(wokalisci);
+        osoby.addAll(pianisci);
+
+        osoby.addAll(saksofonisci);
+        osoby.addAll(technicy);
+        System.out.println(osoby);
+        //lista osoba to po prostu muzycy + technicy z naszych danych
+        //lista powinna byc zsumowana w dokladnie tej kolejnosc!
+
+        final int n = osoby.size(), m = budzet / 100;
+
+        int[][] T = new int[n + 1][m + 1];
+
+        for (int i = 0; i < n + 1; i++)
+            T[i][0] = 0;
+
+        for (int i = 0; i < m + 1; i++)
+            T[0][i] = 0;
+
+        for (int i = 1; i < n + 1; i++)
+            for (int j = 1; j < m + 1; j++) {
+                if ((osoby.get(i - 1)).getStawka() > j)
+                    T[i][j] = T[i][j - 1];
+
+                else
+                    T[i][j] = max(T[i-1][j] , T[i-1][j-osoby.get(i-1).getStawka()] + ((Muzyk)osoby.get(i-1)).getPoziomUmiejetnosci());
+            }
+
+        List <Osoba> wynik = new ArrayList();
+
+            int j=m;
+
+        for (int i=n; i>0; i++)
+            if ( T[i][j] != T[i-1][j] )
+            {
+                j -= osoby.get(i-1).getStawka();
+                wynik.add(osoby.get(i));
+            }
+
+        boolean CzyWszystkieFunkcje = ( wynik.get(0) instanceof Saksofonista && wynik.get(1) instanceof Wokalista && wynik.get(2) instanceof Pianista  && wynik.get(3) instanceof Technik   );
+
+
+        if ( wynik.size() == 4 && CzyWszystkieFunkcje )
+            return new Zespol( wynik.get(3) , wynik.get(1) , wynik.get(2) , wynik.get(0) );
+
+        else
+            return null;
     }
 
 	
